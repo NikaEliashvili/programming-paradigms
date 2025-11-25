b5b50b09418e49115a01f9c666f0bbd4c2dc0fdc
NikaEliashvili <nikaeliashvili18@outlook.com>
2025-11-25 19:53:10 +0400

    task_3

---
+++ delegatebi/task_2/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    delegate void Delegati(int[,] masivi);
+    internal class Class1
+    {
+
+        public void Method_1(int[,] masivi)
+        {
+            for(int i=0; i<masivi.GetLength(0); i++)
+                for (int j = 0; j < masivi.GetLength(1); j++)
+                    if (masivi[i, j] % 2 == 1) masivi[i, j] *= 3;
+        
+        }
+
+        public void Method_2(int[,] masivi)
+        {
+            for (int i = 0; i < masivi.GetLength(0); i++)
+                for (int j = 0; j < masivi.GetLength(1); j++)
+                    if (masivi[i, j] % 2 == 0) masivi[i, j] *= 2;
+        }
+
+        public void Method_3(int[,] masivi)
+        {
+            for (int i = 0; i < masivi.GetLength(0); i++)
+                for (int j = 0; j < masivi.GetLength(1); j++)
+                   masivi[i, j] *= 10;
+        }
+
+        public void Method_4(int[,] masivi)
+        {
+            for (int i = 0; i < masivi.GetLength(0); i++)
+                for (int j = 0; j < masivi.GetLength(1); j++)
+                    masivi[i, j] += 5;
+        }
+
+    }
+}
---
+++ delegatebi/task_2/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[,] masivi = new int[,] { 
+                { 1, 2, 3 }, 
+                { 4, 5, 6 }, 
+                { 7, 8, 9 }, 
+                { 10, 11, 12 } 
+            };
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+
+            Class1 obj = new Class1();
+
+            Delegati delegati;
+            Delegati delegati_1 = new Delegati(obj.Method_1);
+            Delegati delegati_2 = new Delegati(obj.Method_2);
+            Delegati delegati_3 = new Delegati(obj.Method_3);
+            Delegati delegati_4 = new Delegati(obj.Method_4);
+
+            for (int i = 0; i < masivi.GetLength(0); i++)
+            {
+                for (int j = 0; j < masivi.GetLength(1); j++)
+                    label1.Text += masivi[i, j].ToString() + "     ";
+                label1.Text += "\n";
+            }
+
+            delegati = delegati_1;
+            delegati += delegati_2;
+            delegati += delegati_3;
+            delegati(masivi);
+
+            for (int i = 0; i < masivi.GetLength(0); i++)
+            {
+                for (int j = 0; j < masivi.GetLength(1); j++)
+                    label2.Text += masivi[i, j].ToString() + "     ";
+                label2.Text += "\n";
+            }
+
+            delegati -= delegati_1;
+            delegati -= delegati_3;
+            delegati += delegati_4;
+            delegati(masivi);
+
+            for (int i = 0; i < masivi.GetLength(0); i++)
+            {
+                for (int j = 0; j < masivi.GetLength(1); j++)
+                    label3.Text += masivi[i, j].ToString() + "     ";
+                label3.Text += "\n";
+            }
+
+        }
+    }
+}
