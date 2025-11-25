f330194511b01e3114dfbecbd2f0075c02f457ba
NikaEliashvili <nikaeliashvili18@outlook.com>
2025-11-25 17:33:18 +0400

    task_3

---
+++ memkvidreobitoba/task_3/Figure.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    internal class Figure
+    {
+        public int gverdi_1, gverdi_2, gverdi_3;
+        public Figure(int gverdi_1)
+        {
+            this.gverdi_1 = gverdi_1; 
+        }
+
+        public Figure(int gverdi_1, int gverdi_2, int gverdi_3)
+        {
+            this.gverdi_1 = gverdi_1;
+            this.gverdi_2 = gverdi_2;
+            this.gverdi_3 = gverdi_3;
+        }
+
+    }
+
+    internal class Samkutxedi : Figure
+    {
+        public int perimetri;
+        public Samkutxedi(int gverdi_1, int gverdi_2, int gverdi_3) : 
+            base(gverdi_1, gverdi_2, gverdi_3)
+        {
+            perimetri = gverdi_1 + gverdi_2 + gverdi_3;
+        }
+    }
+
+    internal class Kvadrati : Figure
+    {
+        public int fartobi;
+        public Kvadrati(int gverdi_1) :
+            base(gverdi_1)
+        {
+            fartobi = gverdi_1 * gverdi_1;
+        }
+    }
+
+}
--- memkvidreobitoba/task_3/Form1.cs
+++ memkvidreobitoba/task_3/Form1.cs
@@ -16,5 +16,26 @@ namespace task_3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int gv_1, gv_2, gv_3, samk_perimetri, kv_fartobi;
+
+            gv_1 = int.Parse(textBox1.Text);
+            gv_2 = int.Parse(textBox2.Text);
+            gv_3 = int.Parse(textBox3.Text);
+
+
+            Samkutxedi samk_obj = new Samkutxedi(gv_1, gv_2, gv_3);
+            Kvadrati kv_obj = new Kvadrati(gv_1);
+
+            samk_perimetri = samk_obj.perimetri;
+            kv_fartobi = kv_obj.fartobi;
+
+            label4.Text = "სამკუთხედის პერიმეტრი: " + samk_perimetri.ToString() + "\n"
+                + "კვადრატის ფართობი: " + kv_fartobi.ToString();
+
+
+        }
     }
 }
