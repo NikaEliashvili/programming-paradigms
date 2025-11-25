59faf69fb134c4a80a1f22e84b6935e8bc6b6dd0
NikaEliashvili <nikaeliashvili18@outlook.com>
2025-11-25 18:15:43 +0400

    task_4

---
+++ memkvidreobitoba/task_4/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double speed, hours, energyPerKm, km, distance, usedEnergy;
+
+            speed = double.Parse(textBox1.Text);
+            hours = double.Parse(textBox2.Text);
+            energyPerKm = double.Parse(textBox3.Text); 
+
+            Memkvidre_1 obj_1 = new Memkvidre_1(speed, hours);
+            Memkvidre_2 obj_2 = new Memkvidre_2(energyPerKm, speed, hours);
+
+            distance = obj_1.distance;
+            usedEnergy = obj_2.usedEnergy;
+
+            label5.Text = "გავლილი მანძილი: " + distance.ToString() +
+                "\n დახარჯული ენერგია: " + usedEnergy.ToString();
+
+        }
+    }
+}
---
+++ memkvidreobitoba/task_4/Matarebeli.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4
+{
+    internal class Matarebeli
+    {
+        protected double speed, hours;
+
+        public Matarebeli(double speed, double hours)
+        {
+            this.speed = speed;
+            this.hours = hours;
+        }
+
+    }
+
+    internal class Memkvidre_1:Matarebeli
+    {
+        public double distance;
+        public Memkvidre_1(double speed, double hours) :base(speed, hours) {
+            distance = this.speed * this.hours;
+        }
+    }
+
+    internal class Memkvidre_2 : Memkvidre_1
+    {
+        public double usedEnergy;
+        public Memkvidre_2(double energyPerKM, double speed, double hours) :
+            base(speed, hours)
+        {
+            usedEnergy = energyPerKM * this.distance;
+        }
+    }
+
+}
