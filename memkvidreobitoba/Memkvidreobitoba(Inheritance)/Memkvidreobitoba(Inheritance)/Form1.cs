49c8be24db01679bfbc9cf67a84b1fe041bfb739
NikaEliashvili <nikaeliashvili18@outlook.com>
2025-11-25 15:14:58 +0400

    inheritance task 1.

---
+++ memkvidreobitoba/Memkvidreobitoba(Inheritance)/Memkvidreobitoba(Inheritance)/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memkvidreobitoba_Inheritance_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int gverdi_1, gverdi_2, gverdi_3, kv_p, kv_area, mart_p, mart_area, samk_p;
+            double samk_area;
+
+            gverdi_1 = int.Parse(textBox1.Text);
+            gverdi_2 = int.Parse(textBox2.Text);
+            gverdi_3 = int.Parse(textBox3.Text);
+
+            Kvadrati kv_obj = new Kvadrati(gverdi_1);
+            Martkutxedi mart_obj = new Martkutxedi(gverdi_1,gverdi_2);
+            Samkutxedi samk_obj = new Samkutxedi(gverdi_1, gverdi_2, gverdi_3);
+
+            kv_p = kv_obj.GetPerimeterKvadrati();
+            kv_area = kv_obj.GetAreaKvadrati();
+
+            mart_p = mart_obj.GetPerimeterMartkutxedi();
+            mart_area = mart_obj.GetAreaMartkutxedi();
+
+            samk_p = samk_obj.GetPerimeterSamkutxedi();
+            samk_area = samk_obj.GetAreaSamkutxedi();
+
+            label4.Text = "კვადრატის პერიმეტრი: " + kv_p.ToString() + 
+                ", " + "ფართობი: " + kv_area.ToString();
+            label5.Text = "მართკუთხედის პერიმეტრი: " + mart_p.ToString() +
+                ", " + "ფართობი: " + mart_area.ToString();
+            label6.Text = "სამკუთხედის პერიმეტრი: " + samk_p.ToString() +
+                ", " + "ფართობი: " + samk_area.ToString();
+
+
+
+        }
+    }
+}
---
+++ memkvidreobitoba/Memkvidreobitoba(Inheritance)/Memkvidreobitoba(Inheritance)/Kvadrati.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memkvidreobitoba_Inheritance_
+{
+    internal class Kvadrati
+    {
+        public int gverdi_1;
+        protected int perimeter;
+        private int area;
+
+        public Kvadrati(int gverdi_1)
+        {
+            this.gverdi_1 = gverdi_1;
+        }
+
+        public int GetPerimeterKvadrati() {
+            this.perimeter = 4 * this.gverdi_1;
+            return this.perimeter;    
+        }
+
+        public int GetAreaKvadrati()
+        {
+            this.area = this.gverdi_1 * this.gverdi_1;
+            return this.area;
+        }
+
+    }
+
+
+    internal class Martkutxedi : Kvadrati
+    {
+        public int gverdi_2;
+        private int area;
+        public Martkutxedi(int gverdi_1, int gverdi_2):base(gverdi_1)
+        {
+            this.gverdi_2 = gverdi_2;
+        }
+
+        public int GetPerimeterMartkutxedi()
+        {
+            this.perimeter = 2 * (this.gverdi_1 + this.gverdi_2);
+            return this.perimeter;
+        }
+
+        public int GetAreaMartkutxedi()
+        {
+            this.area = this.gverdi_1 * this.gverdi_2;
+            return this.area;
+        }
+
+    }
+
+    internal class Samkutxedi : Martkutxedi
+    {
+        public int gverdi_3;
+        private double area;
+
+        public Samkutxedi(int gverdi_1, int gverdi_2, int gverdi_3):base(gverdi_1, gverdi_2)
+        {
+            this.gverdi_3 = gverdi_3;
+        }
+
+        public int GetPerimeterSamkutxedi()
+        {
+           this.perimeter = this.gverdi_1 + this.gverdi_2 + this.gverdi_3;
+            return this.perimeter;
+        }
+
+        public double GetAreaSamkutxedi()
+        {
+            this.area = (this.gverdi_1 * this.gverdi_2) / 2.0;
+            return this.area;
+        }
+
+
+    }
+
+
+}
