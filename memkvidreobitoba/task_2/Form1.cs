77da5589461c77d1e5a07d9b32d6e1530b872c6d
NikaEliashvili <nikaeliashvili18@outlook.com>
2025-11-25 17:08:43 +0400

    inheritance task 2

---
+++ memkvidreobitoba/task_2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int gverdi_1, gverdi_2, gverdi_3, perimetri;
+            double area;
+
+            gverdi_1 = int.Parse(textBox1.Text);
+            gverdi_2 = int.Parse(textBox2.Text);
+            gverdi_3 = int.Parse(textBox3.Text);
+
+
+            SamkutxediChild samk_obj = new SamkutxediChild(gverdi_1,
+                gverdi_2,
+                gverdi_3);
+
+            perimetri = samk_obj.GetPerimeter();
+            area = samk_obj.GetArea();
+
+            label4.Text = "პერიმეტრი: " + perimetri.ToString() +
+                ", ფართობი: " + area.ToString();
+
+
+        }
+    }
+}
---
+++ memkvidreobitoba/task_2/Samkutxedi.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    internal class Samkutxedi
+    {
+        protected int gverdi_1, gverdi_2, gverdi_3;
+
+        public Samkutxedi(int gverdi_1, int gverdi_2, int gverdi_3)
+        {
+            this.gverdi_1 = gverdi_1;
+            this.gverdi_2 = gverdi_2;
+            this.gverdi_3 = gverdi_3;
+        }
+
+    }
+
+
+    internal class SamkutxediChild : Samkutxedi
+    {
+        private int perimeter;
+
+        public SamkutxediChild(int gverdi_1, int gverdi_2, int gverdi_3)
+            :base(gverdi_1, gverdi_2, gverdi_3)
+        {
+
+        }
+
+        public double GetArea()
+        {
+            return (this.gverdi_1 * this.gverdi_2) / 2.0;
+        }
+
+        public int GetPerimeter()
+        {
+            this.perimeter = this.gverdi_1 + this.gverdi_2 + this.gverdi_3;
+            return this.perimeter;
+        }
+
+    }
+
+}
