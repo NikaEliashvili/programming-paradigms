c4e7c2decdbfa6a9d9552e0eb878683900aec908
NikaEliashvili <nikaeliashvili18@outlook.com>
2025-11-26 16:26:09 +0400

    failebi

---
+++ failebi/task_1/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace task_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            label1.Text = "";
+
+            // 1) --- Create file and write 5 byte numbers ---
+            byte[] nums = { 15, 7, 100, 33, 9 }; 
+
+            FileStream f_out = new FileStream("task_file.dat", FileMode.Create);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                f_out.WriteByte(nums[i]);
+            }
+
+            f_out.Close();
+
+
+            // 2) --- Open file again and read 5 bytes into array ---
+            byte[] read_nums = new byte[5];
+
+            FileStream f_in = new FileStream("task_file.dat", FileMode.Open);
+
+            int value;
+            int index = 0;
+
+            for (; ; )
+            {
+                value = f_in.ReadByte();
+                if (value == -1) break;
+
+                if (index < read_nums.Length)
+                    read_nums[index++] = (byte)value;
+            }
+
+            f_in.Close();
+
+
+            // 3) --- Find minimum value ---
+            byte min = read_nums.Min();
+
+
+            // 4) --- Append minimum to the same file ---
+            FileStream f_append = new FileStream("task_file.dat", FileMode.Append);
+            f_append.WriteByte(min);
+            f_append.Close();
+
+
+            // 5) --- Display results in label ---
+            label1.Text += "Original numbers:\n";
+            for (int i = 0; i < read_nums.Length; i++)
+                label1.Text += read_nums[i] + "   ";
+
+            label1.Text += "\n\nMinimum added to file: " + min;
+        }
+    }
+}
---
+++ failebi/task_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("task_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task_1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9d95a2a8-7b63-4f23-a41e-8306c87ff9a7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
