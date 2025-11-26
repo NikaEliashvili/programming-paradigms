bab1f937a2081ea6eddddae84f64c7f63563d61a
NikaEliashvili <nikaeliashvili18@outlook.com>
2025-11-26 13:27:27 +0400

    generatori

---
+++ generatori/task_1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] random_nums = new int[10];
+            System.Random generator = new System.Random();
+            
+            label1.Text = "";
+
+
+            for (int i = 0; i < random_nums.Length; i++)
+                random_nums[i] = generator.Next(20);
+
+            for (int i = 0; i < random_nums.Length; i++)
+                label1.Text += random_nums[i].ToString() + "  " ;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double[] random_nums = new double[10];
+            System.Random generator = new System.Random();
+
+            label1.Text = "";
+
+            for (int i = 0; i < random_nums.Length; i++)
+                random_nums[i] = Math.Round(generator.NextDouble() - 0.5, 3);
+
+            for (int i = 0; i < random_nums.Length; i++)
+                label1.Text += random_nums[i].ToString() + "  ";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            byte[] random_nums = new byte[10];
+            Random generator = new Random();
+
+            label1.Text = "";
+
+            generator.NextBytes(random_nums);
+
+            for (int i = 0; i < random_nums.Length; i++)
+                label1.Text += random_nums[i].ToString() + "  ";
+        }
+    }
+}
---
+++ generatori/task_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("task_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task_1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eb7b2a09-58e8-4949-9cbb-ff3228823d35")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
